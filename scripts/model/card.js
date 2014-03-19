2d073e3a765001ab94e5c8f104a1f42f3d211c71
Marlolo <marcelo.lopes.lotufo@gmail.com>
2014-03-19 15:08:22 -0300

    Create cardModel
    Abstract card controlling so it can be used to other games and be more organized

---
+++ scripts/model/card.js
@@ -0,0 +1,43 @@
+define([], function()
+{
+	return function Card(data)
+	{
+		var name = data.name;
+		var rarity = data.rarity;
+		var type = data.type;
+		var race = data.race;
+		var clas = data.clas;
+		var mana = data.mana;
+		var attack = data.attack;
+		var health = data.health;
+		var descr = data.descr;
+		var image = data.image;
+		var used = false;
+
+		return {
+			name: name,
+			rarity: rarity,
+			type: type,
+			race: race,
+			clas: clas,
+			mana: mana,
+			attack: attack,
+			health: health,
+			descr: descr,
+			image: image,
+			
+			use: function()
+			{
+				used = true;
+			},
+			reset: function()
+			{
+				used = false;
+			},
+			isUsed: function()
+			{
+				return used;
+			}
+		}
+	};
+});
